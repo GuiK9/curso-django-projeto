c3c70a60adaefbb2c18523b9cf51594745d96d55
Guilherme Sousa <guilherme.tec1107@gmail.com>
2024-08-11 15:47:54 -0300

    Register tests, static links and media location configurations

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,15 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Category
+from .models import Recipe
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategoryAdmin)
