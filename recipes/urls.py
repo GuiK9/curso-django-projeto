2917d06fd3d2147dc9c22d618af12ab934f4f2a8
Guilherme Sousa <guilherme.tec1107@gmail.com>
2024-08-11 20:24:33 -0300

    filters and database searchers

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,6 +6,7 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name="home"),  # Home
-    path('recipe/<int:id>/', views.recipe, name="recipe"),  # Recipe
+    path('recipes/category/<int:category_id>/', views.category, name="category"),  # category
+    path('recipes/<int:id>/', views.recipe, name="recipe"),  # Recipe
 
 ]
