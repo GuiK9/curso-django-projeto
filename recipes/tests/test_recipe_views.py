9a34450e7fcb34b9f41fc2700a9b8e63c966b91d
Guilherme Sousa <guilherme.tec1107@gmail.com>
2024-08-18 15:09:18 -0300

    more tests and optimizations

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,10 +1,9 @@
-from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
-from recipes.models import Category, Recipe, User
+from .test_recipe_base import RecipeTestBase
 
 
-class RecipeViewsTest(TestCase):
+class RecipeViewsTest(RecipeTestBase):
     def test_recipe_home_view_is_correct(self):
         view = resolve(
             reverse('recipes:home')
@@ -27,35 +26,14 @@ class RecipeViewsTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_recipe_home_template_loads_recipes(self):
-        category = Category.objects.create(name='Category')
-        author = User.objects.create_user(
-            first_name='user',
-            last_name='name',
-            username='name',
-            password='123456',
-            email='user',
-        )
-        recipe = Recipe.objects.create(
-            category=category,
-            author=author,
-            title='Recipe Title',
-            description='Recipe description',
-            slug='recipe-slug',
-            preparation_time=10,
-            preparation_time_unit='minutos',
-            servings=5,
-            servings_unit='Porções',
-            preparation_steps='Recipe preparation steps',
-            preparation_steps_is_html=False,
-            is_published=True,
-        )
+        RecipeTestBase().make_recipe()
+
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
-        response_contex_reciopes = response.context['recipes']
-        self.assertIn(recipe.title, content)
-        self.assertIn('10 minutos', content)
-        self.assertIn('5 Porções', content)
-        self.assertEqual(len(response_contex_reciopes), 1)
+        response_contex_recipes = response.context['recipes']
+
+        self.assertIn('Recipe Title', content)
+        self.assertEqual(len(response_contex_recipes), 1)
 
     def test_recipe_home_view_loads_correct_template(self):
         response = self.client.get(reverse('recipes:home'))
