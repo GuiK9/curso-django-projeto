cde907df1a75a7f7fe462194a30e6a4230ec7f4f
Guilherme Sousa <guilherme.tec1107@gmail.com>
2024-08-20 20:46:18 -0300

    tests defaults and parameterized

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,5 +1,8 @@
 from django.core.exceptions import ValidationError
+
+from recipes.models import Recipe
 from .test_recipe_base import RecipeTestBase
+from parameterized import parameterized
 
 
 class RecipeModelTest(RecipeTestBase):
@@ -7,7 +10,40 @@ class RecipeModelTest(RecipeTestBase):
         self.recipe = self.make_recipe()
         return super().setUp()
 
-    def test_recipe_raises_errors_if_title_has_more_than_65_chars(self):
-        self.recipe.title = 'A' * 70
+    def make_recipe_no_default(self):
+        recipe = Recipe(
+            category=self.make_category(name="teste_default_category"),
+            author=self.make_author(username="newusername"),
+            title='Recipe Title',
+            description='Recipe description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe preparation steps',
+            )
+        return recipe
+
+    @parameterized.expand([
+            ('title', 65),
+            ('description', 165),
+            ('preparation_time_unit', 65),
+            ('servings_unit', 65),
+        ])
+    def test_recipe_fields_max_length(self, field, max_length):
+        setattr(self.recipe, field, 'A' * (max_length + 1))
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
+
+    def test_recipe_preparation_time_is_html_is_false_by_default(self):
+        recipe = self.make_recipe_no_default()
+        recipe.full_clean()
+        recipe.save()
+        self.assertFalse(recipe.preparation_steps_is_html)
+
+    def test_recipe_is_published_is_false_by_default(self):
+        recipe = self.make_recipe_no_default()
+        recipe.full_clean()
+        recipe.save()
+        self.assertFalse(recipe.is_published)
