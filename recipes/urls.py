0a886450f7f73c4c787eb54a73252b8d9ea8e0cb
Guilherme Sousa <guilherme.tec1107@gmail.com>
2025-02-22 06:41:31 -0300

    Using cbv to home

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.home, name="home"),  # Home
+    path('', views.RecipeListViewBase.as_view(), name="home"),  # Home
     path('recipes/search/', views.search, name="search"),  # url
     path('recipes/category/<int:category_id>/', views.category, name="category"),  # category
     path('recipes/<int:id>/', views.recipe, name="recipe"),  # Recipe
